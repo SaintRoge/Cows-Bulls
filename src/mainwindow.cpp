59bad485b3f9fecd72b8ec02b5d284693994f493
SaintRoge <lemondesansfil@gmail.com>
2017-01-01 19:10:24 +0100

    Menu improved

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,8 +2,27 @@
 
 MainWindow::MainWindow(sf::RenderWindow *window) {
   m_window = window;
+
+  std::cout << "Menu created" << std::endl;
+
+  m_menuArraySize = 2;
+
+  std::string menuStringArray[2] = {"Play", "Quit"};
+
+  for (int j(0); j < m_menuArraySize; j++) {
+	m_menuStringArray.push_back(menuStringArray[j]);
+  }
+  
   if (!m_font.loadFromFile("fonts/joystix.ttf")) {
 	std::cout << "Font have not been loaded" << std::endl;
+  }
+
+  for (int i(0); i < m_menuArraySize; i++) {
+	m_menuArray.push_back(sf::Text());
+	m_menuArray[i].setCharacterSize(60);
+	m_menuArray[i].setString(m_menuStringArray[i]);
+	m_menuArray[i].setPosition(m_window->getSize().x/2 - 100, 200 + 100 * i);
+	m_menuArray[i].setFont(m_font);
   }
 }
 
@@ -11,7 +30,7 @@ MainWindow::~MainWindow() {
 
 }
 
-void MainWindow::start() {  
+void MainWindow::start() {
   while (m_window->isOpen()) {
 	while (m_window->pollEvent(m_event)) {
 	  if (m_event.type == sf::Event::Closed) {
@@ -21,6 +40,8 @@ void MainWindow::start() {
 	  }
 	  if (m_event.type == sf::Event::Resized) {
 		m_window->setView(sf::View(sf::FloatRect(0, 0, m_event.size.width, m_event.size.height)));
+		m_playText.setPosition(m_window->getSize().x/2 - 100, 200);
+		m_quitText.setPosition(m_window->getSize().x/2 - 100, 300);
 	  }
 	}
 
@@ -28,7 +49,16 @@ void MainWindow::start() {
 	  m_game = new Game(m_window);
 	  m_game->start();
 	}
+
+	
 	m_window->clear(sf::Color(109, 153, 255));
+
+	for (int i(0); i < m_menuArraySize; i++) {
+	  m_window->draw(m_menuArray[i]);
+	}
+	
 	m_window->display();
   }
 }
+
+
--- src/mainwindow.hpp
+++ src/mainwindow.hpp
@@ -3,6 +3,7 @@
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
 
 #include "game.hpp"
 
@@ -16,7 +17,17 @@ public:
    * Starts the game
    * Returns nothing
    */
+  void resize();
+  /*
+   * Needs nothing
+   * Resize
+   * Returns nothing
+   */
 private:
+  int m_menuArraySize;
+  
+  std::vector<sf::Text> m_menuArray;
+  std::vector<std::string> m_menuStringArray;
   sf::RenderWindow *m_window;
   sf::Event m_event;
   sf::Font m_font;
